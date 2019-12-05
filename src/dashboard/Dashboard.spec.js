c1796fb6756ac0b02bea75127b5d62736aae9e89
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-12-04 20:20:53 -0800

    finished MVP thanks to that zoom group

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+import React from "react";
+import * as rtl from "@testing-library/react";
+import Dashboard from "./Dashboard";
+import "@testing-library/jest-dom/extend-expect";
+
+test("shows the controls and display", () => {
+  const wrapper = rtl.render(<Dashboard />);
+
+  expect(wrapper.asFragment()).toMatchSnapshot();
+});
